3794911f6cbc7ef813c6087de5185f4270016248
honeybee <honeybee@lab4dx.co.kr>
2022-11-30 14:39:17 +0900

    apply prettier

--- src/component/theme/StyledTheme.js
+++ src/component/theme/StyledTheme.js
@@ -1,33 +1,33 @@
 const StyledTheme = {
-    spacing: 8,
-    base: {
-        header: {
-            height: 50,
-            headerColor: '#f3ece5',
+  spacing: 8,
+  base: {
+    header: {
+      height: 50,
+      headerColor: "#f3ece5",
+    },
+    section: {
+      backgroundColor: "#5bc4ca",
+    },
+    button: {
+      backgroundColor: "#c1c1c1",
+      hoverButtonColor: "#d4d3d1",
+      labelColor: "#1e1e1e",
+      labelSize: 16,
+    },
+  },
+  variables: {
+    fontSize: 14,
+    titleDivHeight: 300,
+    contentDivWidth: 1500,
+  },
+  muiTheme: {
+    overrides: {
+      MuiIconButton: {
+        colorPrimary: {
+          color: "#dddcda",
         },
-        section: {
-            headerColor: '#5bc4ca',
-        },
-        button: {
-            backgroundColor: '#c1c1c1',
-            hoverButtonColor: '#d4d3d1',
-            labelColor: '#1e1e1e',
-            labelSize: 16,
-        }
+      },
     },
-    variables: {
-        fontSize: 14,
-        sideWidth: 15,
-    },
-    muiTheme: {
-        overrides:{
-            MuiIconButton: {
-                colorPrimary: {
-                color: '#dddcda',
-                },
-            },
-        }
-
-    },
-}
+  },
+};
 export default StyledTheme;
